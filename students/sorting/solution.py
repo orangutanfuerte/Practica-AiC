0f316a3e8c0c054e06df13559b77bdeebffb8226
Aleix Ràfols <arb31@alumnes.udl.cat>
2026-04-02 11:54:08 +0200

    merge-sort prova

--- students/sorting/solution.py
+++ students/sorting/solution.py
@@ -3,10 +3,45 @@ import bz2
 import json
 import sys
 from pathlib import Path
+import math
+
+
+def merge(A, m):
+    n = len(A)
+    B = [0] * n
+    
+    i = 0
+    j = m+1
+    # recorrem el array
+    for k in range(n):
+        if j >= n: # si j està fora de la mida de l'array
+            B[k] = A[i]
+            i += 1
+        elif i > m:
+            B[k] = A[j]
+            j += 1
+        elif A[i] < A[j]:
+            B[k] = A[i]
+            i += 1
+        else:
+            B[k] = A[j]
+            j += 1
+    return B
+
+
+def mergesort(A, start, end):
+    if start < end:
+        m = math.floor((start+end)/2)
+        mergesort(A, start, m)
+        mergesort(A, m+1, end)
+        A[start:end+1] = merge(A[start:end+1], m-start)
+        #print(A, start, end, m)
 
 
 def my_sort(arr):
-    raise NotImplementedError("Implement Me!")
+    mergesort(arr, 0, len(arr)-1)
+    return arr
+
 
 
 def main():
